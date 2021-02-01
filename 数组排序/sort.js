36ec193532be96ee238dd244dfc06220fcce321a
xu-xiaoya <1525281916@qq.com>
2021-02-01 15:04:44 +0800

    sort

---
+++ 数组排序/sort.js
@@ -0,0 +1,10 @@
+function reverse() {
+    var a = [1,2,3,4,5];
+    a.reverse();
+    document.write("reverse："+a);
+}
+function _sort() {
+    var a = ["a","e","b","c","g"];
+    a.sort();
+    document.write("sort:" +a);
+}
