30f0c98e6d31d8997194fe195824e5928d407c3c
xu-xiaoya <1525281916@qq.com>
2021-02-17 20:57:56 +0800

    this

---
+++ 函数中的this/函数引用和调用.js
@@ -0,0 +1,21 @@
+var o = {
+    name: "o",
+    f: function() {
+        return this;
+    }
+}
+/*函数引用：改变函数的执行作用域*/
+o.o1 = {
+    name: "o1",
+    me : o.f        //引用对象o的方法f
+}
+var who = o.o1.me();
+alert(who.name);    //o1, 说明this代表对象o1
+
+/*函数调用：不改变函数的执行作用域 */
+o.o1 = {
+    name: "o1",
+    me : o.f()        //调用对象o的方法f
+}
+var who = o.o1.me;
+alert(who.name);    //o, 说明this代表对象o
