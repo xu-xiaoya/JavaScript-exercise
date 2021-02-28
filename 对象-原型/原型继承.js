5135059ddc3e83b7765c159d4e8501aa10cf383f
xu-xiaoya <1525281916@qq.com>
2021-02-28 23:18:54 +0800

    原型继承

---
+++ 对象-原型/原型继承.js
@@ -0,0 +1,36 @@
+/*原型继承：通过复制已经存在的原型对象来实现继承关系的
+
+优点：结构简练，不需要每次构造都调用父类的构造函数
+缺点：
+①每个类型只有一个原型，所以他不直接支持多重继承
+②不能很好的支持多参数或者动态参数的父类
+*/
+
+function A(x) {     //A类
+    this.x1 = x;       //本地属性及方法
+    this.get1 = function() {
+        return this.x1;
+    }
+}
+function B(x) {     //B类
+    this.x2 = x;       //本地属性及方法
+    this.get2 = function() {
+        return this.x2 + this.x2;
+    };
+}
+B.prototype = new A(1);     //原型对象继承A的实例
+
+function C(x) {     //C类
+    this.x3 = x;       //本地属性及方法
+    this.get3 = function() {
+        return this.x3 * this.x3;
+    };
+}
+C.prototype = new B(2);
+
+var b = new B(2);
+var c = new C(3);
+alert(b.x1);//在实例对象b中调用A的属性x1，1
+alert(c.x1);//在实例对象c中调用A的属性x1，1
+alert(c.get3());//在实例对象c中调用C的方法get3()，9
+alert(c.get2());//在实例对象c中调用B的方法get2()，4
