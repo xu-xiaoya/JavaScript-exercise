64f8716527a2924e6b57dbe166931d28047dd215
xu-xiaoya <1525281916@qq.com>
2021-03-18 20:03:13 +0800

    异步

---
+++ DOM（文档对象模型）/使用范围/清除范围.js
@@ -0,0 +1,2 @@
+range.detach();     //从文档中分离
+range = null;       //解除引用
---
+++ DOM（文档对象模型）/实战案例/异步交互/传递参数/server_code.js
@@ -0,0 +1,3 @@
+var  queryString = location.search.substring(1);//是截取当前 url 地址？后面所有的内容
+alert(queryString); 
+//提示为空，服务器没有接收到这个参数
