9f9460bb6d4b2537936cb4a862feca24281caf23
xu-xiaoya <1525281916@qq.com>
2021-05-11 17:38:57 +0800

    引号

--- JS语法基础/运算符/反引号.js
+++ JS语法基础/运算符/反引号.js
@@ -1,4 +1,5 @@
 //``反引号，保留格式
+var data = {name:"小明", age:"18"};
 var _html1 = `<div class= 'content'>我的名字叫${data.name},
 我今年的年龄是${data.age}</div>`;
 console.log(_html1);
--- JS语法基础/运算符/短路逻辑.js
+++ JS语法基础/运算符/短路逻辑.js
@@ -8,3 +8,16 @@ var age = 11;
 age >= 18 && console.log('成年人');
 
 age > 2 || console.log(age); 
+
+
+
+//短路逻辑赋值
+//如果没有执行第二个表达式，就把第一个表达式赋给a
+//如果执行到了第二个表达式，就把第二个表达式赋给a
+var a = 0||1;//a为1，不是true
+var a = 0&&1;//a为0，不是false
+console.log(a);
+
+var username;
+var res = username||'游客';//游客
+console.log(res);
