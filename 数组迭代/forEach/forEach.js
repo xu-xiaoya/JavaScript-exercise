be10a5bc1d7832332e32c9c9d47bf4271e6dfb95
xu-xiaoya <1525281916@qq.com>
2021-05-29 09:18:06 +0800

    foreach

--- 数组迭代/forEach/forEach.js
+++ 数组迭代/forEach/forEach.js
@@ -1,10 +1,9 @@
-function showResults(value, index, ar) {
-    document.write("value：" + value);
-    document.write("  index：" + index);
-    document.write("<br/>");
+function showResults(value, index, a) {
+   console.log(`
+    value：${value}
+    index：${index}`);
 }
-function foreach1() {
-    var letters = ['a','b','c'];
-    letters.forEach(showResults);
-    document.write("<br/>");
-}
+
+//可以forEach遍历使用一个函数方法
+var letters = ['a','b','c'];
+letters.forEach(showResults);
--- 数组迭代/forEach/foreach_sum.js
+++ 数组迭代/forEach/foreach_sum.js
@@ -1,13 +1,19 @@
-function foreach_sum() {
-    var a = [10,11,12];
-    for (i in a) {
-        document.write(a[i]+ ",");
-    }
-    var sum = 0;
-    a.forEach(
-        function addvalue(value) {
-            sum += value;
-        }
-    );
-    document.write("sum : "  + sum + '<br/>');
+var a = [10,11,12];
+
+//for-in方法
+for (i in a) {
+    console.log(a[i]+ ",");
 }
+
+a.forEach((a,b)=>{
+    console.log(a);//a值
+    console.log(b);//b下标
+})
+
+var sum = 0;
+a.forEach(
+    function addvalue(value) {
+        sum += value;
+    }
+);
+console.log("sum : "  + sum );
